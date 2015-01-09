72d8abd23deed3b9eff39a1d9172227f3b2f02fe
aishairzay <amit.ishairzay@gmail.com>
2015-01-08 21:07:51 -0500

    Bot that mines stuff in the works

---
+++ teams/minerdraft/Constants.java
@@ -0,0 +1,15 @@
+package minerdraft;
+
+public class Constants {
+	// broadcast channels
+	static int BeaverCount = 0;
+	static int MinerCount = 1;
+	static int BarrackProductionCount = 2;
+	static int BestFarmScore = 3;
+	static int BestFarmX = 4;
+	static int BestFarmY = 5;
+
+	// other constants
+	static int BeaverLimit = 10;
+	static int MinerLimit = 5;
+}
