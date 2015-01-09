55cbb82cad5dd9b052a9afa80e3c36d1184f630c
aishairzay <amit.ishairzay@gmail.com>
2015-01-08 23:14:40 -0500

    starting bugging

--- teams/ridavoy/Soveti.java
+++ teams/ridavoy/Soveti.java
@@ -2,53 +2,41 @@ package ridavoy;
 
 import battlecode.common.*;
 
-public abstract class Soveti
-{
-    protected RobotController rc;
-    protected Team            myTeam;
-    protected Team            enemyTeam;
-    protected MapLocation     allyHQ;
-    protected MapLocation     enemyHQ;
-    protected MapLocation[]   enemyTowers;
-    protected MapLocation[]   allyTowers;
+public abstract class Soveti {
+	protected RobotController rc;
+	protected Team myTeam;
+	protected Team enemyTeam;
+	protected MapLocation allyHQ;
+	protected MapLocation enemyHQ;
+	protected MapLocation[] enemyTowers;
+	protected MapLocation[] allyTowers;
 
+	public Soveti(RobotController myRC) {
+		rc = myRC;
+		allyHQ = rc.senseHQLocation();
+		enemyHQ = rc.senseEnemyHQLocation();
+		enemyTowers = rc.senseEnemyTowerLocations();
+		allyTowers = rc.senseTowerLocations();
+	}
 
-    public Soveti(RobotController myRC)
-    {
-        rc = myRC;
-        allyHQ = rc.senseHQLocation();
-        enemyHQ = rc.senseEnemyHQLocation();
-        enemyTowers = rc.senseEnemyTowerLocations();
-        allyTowers = rc.senseTowerLocations();
-    }
+	public abstract void run();
 
+	public boolean attack() throws GameActionException {
+		RobotInfo[] nearbyEnemies = rc.senseNearbyRobots(
+				rc.getType().attackRadiusSquared, rc.getTeam().opponent());
+		RobotInfo target = null;
 
-    public abstract void run();
-
-
-    public boolean attack()
-        throws GameActionException
-    {
-        RobotInfo[] nearbyEnemies =
-            rc.senseNearbyRobots(rc.getType().attackRadiusSquared, rc.getTeam()
-                .opponent());
-        RobotInfo target = null;
-
-        if (nearbyEnemies.length > 0 && rc.isWeaponReady())
-        {
-            for (RobotInfo ri : nearbyEnemies)
-            {
-                if (target == null || ri.health < target.health)
-                {
-                    target = ri;
-                }
-            }
-            if (rc.canAttackLocation(target.location))
-            {
-                rc.attackLocation(target.location);
-                return true;
-            }
-        }
-        return false;
-    }
+		if (nearbyEnemies.length > 0 && rc.isWeaponReady()) {
+			for (RobotInfo ri : nearbyEnemies) {
+				if (target == null || ri.health < target.health) {
+					target = ri;
+				}
+			}
+			if (rc.canAttackLocation(target.location)) {
+				rc.attackLocation(target.location);
+				return true;
+			}
+		}
+		return false;
+	}
 }
