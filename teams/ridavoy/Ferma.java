68b0108742c705fc171097a338f0859344f73ed4
miraziz <miraziz@vt.edu>
2015-01-08 21:30:09 -0500

    Added serp and ferma

---
+++ teams/ridavoy/Ferma.java
@@ -0,0 +1,22 @@
+package ridavoy;
+
+import battlecode.common.RobotController;
+
+public class Ferma
+    extends Zdaniya
+{
+
+    public Ferma(RobotController rc)
+    {
+        super(rc);
+    }
+
+
+    @Override
+    public void run()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ teams/ridavoy/Serp.java
@@ -0,0 +1,22 @@
+package ridavoy;
+
+import battlecode.common.RobotController;
+
+public class Serp
+    extends Proletariat
+{
+
+    public Serp(RobotController rc)
+    {
+        super(rc);
+    }
+
+
+    @Override
+    public void run()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
