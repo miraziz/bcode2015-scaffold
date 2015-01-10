cc5973476cb6b0437740e899f7f5333803836fa8
aishairzay <amit.ishairzay@gmail.com>
2015-01-10 04:22:10 -0500

    Finds a potential rally area for units to gather around

--- teams/ridavoy/Shtab.java
+++ teams/ridavoy/Shtab.java
@@ -1,9 +1,6 @@
 package ridavoy;
 
-import battlecode.common.Direction;
-import battlecode.common.GameActionException;
-import battlecode.common.RobotController;
-import battlecode.common.RobotType;
+import battlecode.common.*;
 
 public class Shtab
     extends Atakuyushchiy
@@ -13,8 +10,42 @@ public class Shtab
         throws GameActionException
     {
         super(rc);
+
+        MapLocation rallyPoint = findRallyPoint(); // broadcast this location
+        // broadcastLocation(*channel*, rallyPoint);
+
+        /* testing code */
         rc.spawn(Direction.SOUTH, RobotType.BEAVER);
         this.broadcastLocation(1, enemyHQ);
+    }
+
+
+    private MapLocation findRallyPoint()
+    {
+        int xAvg = 0;
+        int yAvg = 0;
+        for (MapLocation loc : this.allyTowers)
+        {
+            xAvg += loc.x;
+            yAvg += loc.y;
+        }
+        xAvg += allyHQ.x;
+        yAvg += allyHQ.y;
+        xAvg /= allyTowers.length + 1;
+        yAvg /= allyTowers.length + 1;
+        MapLocation avg = new MapLocation(xAvg, yAvg);
+        MapLocation closest = allyHQ;
+        int closestDist = closest.distanceSquaredTo(avg);
+        for (MapLocation loc : this.allyTowers)
+        {
+            int dist = loc.distanceSquaredTo(avg);
+            if (dist <= closestDist)
+            {
+                closestDist = dist;
+                closest = loc;
+            }
+        }
+        return closest;
     }
 
 
