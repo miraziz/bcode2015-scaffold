ad684bea201cd9978178acb2fcc587917270d58b
aishairzay <amit.ishairzay@gmail.com>
2015-01-10 02:09:15 -0500

    Made function to help miners mine

--- teams/ridavoy/Atakuyushchiy.java
+++ teams/ridavoy/Atakuyushchiy.java
@@ -1,6 +1,10 @@
 package ridavoy;
 
+import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
+import battlecode.common.RobotInfo;
+import battlecode.common.RobotType;
+import battlecode.common.TerrainTile;
 
 public abstract class Atakuyushchiy
     extends Zdaniya
@@ -9,6 +13,58 @@ public abstract class Atakuyushchiy
     public Atakuyushchiy(RobotController rc)
     {
         super(rc);
+        broadcastScores();
     }
 
+
+    void broadcastScores()
+    {
+        int droneVulnerabilityScore = 0;
+        int vulnerabilityScore = 150;
+        int farmScore = 0;
+        MapLocation[] nearby =
+            MapLocation.getAllMapLocationsWithinRadiusSq(rc.getLocation(), 35);
+        for (MapLocation loc : nearby)
+        {
+            if (rc.canSenseLocation(loc))
+            {
+                TerrainTile tile = rc.senseTerrainTile(loc);
+                if (tile == TerrainTile.NORMAL)
+                {
+                    farmScore += rc.senseOre(loc);
+                }
+                else if (tile == TerrainTile.OFF_MAP)
+                {
+                    droneVulnerabilityScore += 12;
+                    vulnerabilityScore -= 8;
+                }
+                else if (tile == TerrainTile.VOID)
+                {
+                    droneVulnerabilityScore += 6;
+                    vulnerabilityScore -= 4;
+                }
+            }
+            RobotInfo[] allies = rc.senseNearbyRobots(25, myTeam);
+            for (RobotInfo ally : allies)
+            {
+                if (ally.type == RobotType.HQ)
+                {
+                    vulnerabilityScore -= 25;
+                }
+                else if (ally.type == RobotType.TOWER)
+                {
+                    vulnerabilityScore -= 15;
+                }
+            }
+        }
+        if (rc.getType() == RobotType.HQ)
+        {
+            vulnerabilityScore -= 25;
+        }
+        droneVulnerabilityScore += vulnerabilityScore;
+
+        /*
+         * tell others of these scores, broadcast!
+         */
+    }
 }
--- teams/ridavoy/Serp.java
+++ teams/ridavoy/Serp.java
@@ -1,24 +1,77 @@
 package ridavoy;
 
-import battlecode.common.GameActionException;
-import battlecode.common.RobotController;
+import battlecode.common.*;
 
 public class Serp
     extends Proletariat
 {
+    boolean reachedFarm;
+
 
     public Serp(RobotController rc)
         throws GameActionException
     {
         super(rc);
+        reachedFarm = false;
     }
 
 
     @Override
     public void run()
+        throws GameActionException
     {
-        // TODO Auto-generated method stub
 
+        // if not in any danger
+        if (rc.isCoreReady())
+        {
+            if (reachedFarm)
+            {
+                if (rc.canMine() && rc.senseOre(rc.getLocation()) > 0)
+                {
+                    rc.mine();
+                }
+                else
+                {
+                    Direction bestDir = Direction.NORTH;
+                    double bestScore = 0;
+                    Direction dir = Direction.NORTH;
+                    for (int i = 0; i < 8; i++)
+                    {
+                        if (rc.canMove(dir))
+                        {
+                            double oreCount =
+                                rc.senseOre(rc.getLocation().add(dir));
+                            if (oreCount > bestScore)
+                            {
+                                bestDir = dir;
+                                bestScore = oreCount;
+                            }
+                        }
+                        dir = dir.rotateRight();
+                    }
+                    if (bestScore == 0)
+                    {
+                        // set destination to farm spot.
+                        bug();
+                    }
+                    else
+                    {
+                        move(bestDir);
+                    }
+                }
+            }
+            else
+            {
+                if (rc.canMine() && rc.senseOre(rc.getLocation()) > 1)
+                {
+                    rc.mine();
+                }
+                else
+                {
+                    // set destination to a farm spot.
+                    bug();
+                }
+            }
+        }
     }
-
 }
